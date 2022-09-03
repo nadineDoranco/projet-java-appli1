02cb7acd45797a068ae013fee0375143ab311f41
Nadine Doranco <havanueva@gmail.com>
2022-09-03 18:31:43 +0200

    enregistrement utilisateur

---
+++ src/test/java/test/example/autorisationconnection/AutorisationConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package test.example.autorisationconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorisationConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
